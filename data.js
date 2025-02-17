e0d82e1b9bc364c9bfff157440499ff8c9444551
PREDAT0R01 <62991666+PREDAT0R01@users.noreply.github.com>
2025-02-18 02:23:14 +0500

    upload

---
+++ data.js
@@ -0,0 +1,278 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_20250217160428_0339_d",
+      "name": "DJI_20250217160428_0339_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -1.274737490978552,
+        "pitch": 0.003632262392308405,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4441093999590873,
+          "pitch": 0.11585759674013829,
+          "rotation": 0,
+          "target": "0-dji_20250217160428_0339_d"
+        },
+        {
+          "yaw": -1.1019855139954338,
+          "pitch": 0.14398814733725374,
+          "rotation": 0,
+          "target": "0-dji_20250217160428_0339_d"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.138702641254026,
+          "pitch": 0,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.138702641254026,
+          "pitch": 0,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.138702641254026,
+          "pitch": 0,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.1019855139954338,
+          "pitch": 0.14398814733725374,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.1019855139954338,
+          "pitch": 0.14398814733725374,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-dji_20250217160716_0340_d",
+      "name": "DJI_20250217160716_0340_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 2.146605511690715,
+        "pitch": -0.38554599814018964,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.28495171461132074,
+          "pitch": 0.25838181966541995,
+          "rotation": 1.5707963267948966,
+          "target": "5-dji_20250217163140_0344_d"
+        },
+        {
+          "yaw": 0.20784780221724652,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-dji_20250217160716_0340_d"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dji_20250217161352_0341_d",
+      "name": "DJI_20250217161352_0341_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dji_20250217161815_0342_d",
+      "name": "DJI_20250217161815_0342_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-dji_20250217162736_0343_d",
+      "name": "DJI_20250217162736_0343_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dji_20250217163140_0344_d",
+      "name": "DJI_20250217163140_0344_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
